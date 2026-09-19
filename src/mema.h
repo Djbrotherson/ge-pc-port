096e3e8f62932739b22447cef943ba263cbc8702
bitflipunix <bitflipunix@gmail.com>
2026-09-19 19:37:04 +1000

    R36S: make mema addresses LP64-safe

--- src/mema.h
+++ src/mema.h
@@ -10,6 +10,10 @@ void *memaAlloc(u32 size);
 void memaFree(void *addr, s32 size);
 void memaDumpPrePostMerge(void);
 s32 memaGetLongestFree(void);
+#ifdef PORT
+s32 memaRealloc(uintptr_t addr, u32 newsize, u32 oldsize);
+#else
 s32 memaRealloc(s32 addr, u32 newsize, u32 oldsize);
+#endif
 
 #endif
