437c9637321bd8ae34be6e6f9f6c86fc8a897695
bitflipunix <bitflipunix@gmail.com>
2026-09-19 15:10:42 +1000

    ARM64: type favorite-weapon text return as pointer

--- port/include/pc_protos.h
+++ port/include/pc_protos.h
@@ -206,7 +206,7 @@ void free();
  * truncated to int (crashed configGetFrameDump on Linux). */
 char * getenv();
 void * realloc();
-int frontGetPlayersFavoriteWeaponInHand();
+u16 * frontGetPlayersFavoriteWeaponInHand();
 void generate_player_thrown_grenade();
 void generate_player_thrown_knife();
 void generate_player_thrown_object();
