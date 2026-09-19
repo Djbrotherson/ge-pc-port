2782d8e05dcaf897267cfcd4217bd513f7ada192
bitflipunix <bitflipunix@gmail.com>
2026-09-19 22:03:54 +1000

    R36S: rebase Bond move animation offsets pointer-safely

--- src/game/initBondDATAdefaults.c
+++ src/game/initBondDATAdefaults.c
@@ -166,7 +166,7 @@ void sets_a_bunch_of_BONDdata_values_to_default(void)
     {
         sub_GAME_7F0062C0(
             // match hack: addu address calculated backwards
-            (void*)((s32)g_BondMoveAnimationSetup[i].anim_id + (s32)&ptr_animation_table->data),
+            (void *)((u8 *)ptr_animation_table + (u32)g_BondMoveAnimationSetup[i].anim_id),
             (s32)g_BondMoveAnimationSetup[i].loopframe,
             (s32)g_BondMoveAnimationSetup[i].endframe,
             &spD0);
@@ -194,7 +194,7 @@ void sets_a_bunch_of_BONDdata_values_to_default(void)
     modelSetAnimation(
         &g_CurrentPlayer->model,
         // match hack: addu address calculated backwards
-        (struct ModelAnimation *) ((s32)g_BondMoveAnimationSetup[g_CurrentPlayer->headanim].anim_id + (s32)&ptr_animation_table->data),
+        (struct ModelAnimation *)((u8 *)ptr_animation_table + (u32)g_BondMoveAnimationSetup[g_CurrentPlayer->headanim].anim_id),
         0,
         g_BondMoveAnimationSetup[g_CurrentPlayer->headanim].loopframe,
         0.5f,
