8fb2097e32d3c4ce12da98d05a7068f16366659b
bitflipunix-re <bitflipunix@atomicmail.io>
2026-09-24 16:09:21 +1000

    telemetry: add Dam-lab runtime interface

---
+++ work/goldeneye-pc-port/port/include/damlab.h
@@ -0,0 +1,51 @@
+#ifndef GE_DAMLAB_H
+#define GE_DAMLAB_H
+
+#include <PR/ultratypes.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct DamLabSnapshot {
+    uint64_t sample_seq;
+    int stage;
+    int camera_mode;
+    int room;
+    int spawn_index;
+    float pos_x, pos_y, pos_z;
+    float cam_x, cam_y, cam_z;
+    float stan_height;
+    uintptr_t stan;
+    float fps;
+    float cpu_percent;
+    unsigned long rss_kb;
+    unsigned long mem_available_kb;
+    unsigned anomaly_flags;
+} DamLabSnapshot;
+
+enum {
+    DAMLAB_ANOM_SPAWN      = 1u << 0,
+    DAMLAB_ANOM_STAN_NULL  = 1u << 1,
+    DAMLAB_ANOM_POS_NAN    = 1u << 2,
+    DAMLAB_ANOM_POS_JUMP   = 1u << 3,
+    DAMLAB_ANOM_ROOM       = 1u << 4,
+};
+
+void damLabInit(void);
+void damLabGameplayTick(int stage, int camera_mode, int room,
+                        float pos_x, float pos_y, float pos_z,
+                        float cam_x, float cam_y, float cam_z,
+                        float stan_height, uintptr_t stan);
+void damLabRecordSpawn(int spawn_index, float x, float y, float z,
+                       float lx, float ly, float lz, uintptr_t stan);
+void damLabHostSample(float fps);
+const DamLabSnapshot *damLabGetSnapshot(void);
+void damLabFormatOverlay(char *dst, unsigned dst_size);
+void damLabShutdown(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
